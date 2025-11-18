428376924e3621faece8eb3f201fdc780eb4c4e4
shivrajgn7 <shivrajgn7@.com>
2025-11-18 18:48:05 +0530

    commiting dto  and service

--- Movie-ticket-servlet/src/com/xworkz/reg/Register.java
+++ Movie-ticket-servlet/src/com/xworkz/reg/Register.java
@@ -1,5 +1,6 @@
 package com.xworkz.reg;
 
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
@@ -7,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/booking" ,loadOnStartup = 1)
-public class Register extends HttpServlet {
+public class Register extends GenericServlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException {
         String  namee=servletRequest.getParameter("movie");
@@ -32,7 +33,6 @@ public class Register extends HttpServlet {
         printWriter.println("</h1>");
 
         printWriter.println("</html>");
-
 
     }
 }
